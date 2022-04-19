21b9a7854835ef28b1f10ae26754a4286aa81990
Francisco Freire <contact.frann@gmail.com>
2022-04-19 11:54:53 -0300

    Quizz send data

--- examples/js/Feature-send-process-data.js
+++ examples/js/Feature-send-process-data.js
@@ -26,7 +26,10 @@ AFRAME.registerComponent('send-process', {
         var xhr = new XMLHttpRequest();
         xhr.open("POST", "https://api.medixlab.vxr.space/process", true);
         xhr.setRequestHeader('Content-Type', 'application/json');
-        
+        if (this.status == 200) {
+          console.log("response", this.response); // JSON response
+          var processID = this.response.process;
+        }
         xhr.send(JSON.stringify({
           "process_number": process,
           "duration": duration,
