f44592775931879b0728a054ca93c95459ecd083
bryandrm <bryandrm97@gmail.com>
2022-04-18 22:27:33 -0600

    added mixin point configurations, send data script, table pen and file objects for event send data.

---
+++ examples/js/Feature-send-process-data.js
@@ -0,0 +1,45 @@
+AFRAME.registerComponent('send-process', {
+  schema: {
+    keyCode: { default: 32 }
+  },
+  
+
+  init: function () {
+    let boolean = true;
+    let mySelf = this.el;
+    let initProcess = new Date()
+    this.el.addEventListener('collide', function (e) {
+    // console.log({mySelf})
+    // console.log("e.detail: ",e.detail.body.el.id)
+      if (e.detail.body.el.id=="e-Medixlab_Pen") {
+      
+        if(boolean){
+          boolean=false
+       document.getElementById("lhand").removeAttribute("mixin")
+        document.getElementById("rhand").removeAttribute("mixin")
+        document.getElementById("lhand").setAttribute("mixin","point")
+        document.getElementById("rhand").setAttribute("mixin","point")
+        let endProcess = new Date()
+        let duration = endProcess-initProcess
+        let initProcessTime = initProcess.toISOString().replace('Z', '')
+        let endProcessTime = endProcess.toISOString().replace('Z', '')
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://api.medixlab.vxr.space/process", true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        
+        xhr.send(JSON.stringify({
+          "process_number": process,
+          "duration": duration,
+          "process_state": 1,
+          "init_process": initProcessTime,
+          "end_process": endProcessTime,
+          "session_id": localStorage.getItem("sessionID")
+        }));
+        console.log("status: ",xhr.status)
+      }
+      }
+      
+  })
+  
+}
+});
