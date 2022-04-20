1b199c0f2ea037534c6eeaba0d18c446f557314d
Bryandrm <bryandrm97@gmail.com>
2022-04-20 15:57:32 -0600

    added height adjustment feature to process1.

--- examples/js/Feature-send-process-data.js
+++ examples/js/Feature-send-process-data.js
@@ -1,3 +1,4 @@
+let aux_times_used = 0
 AFRAME.registerComponent('send-process', {
   schema: {
     keyCode: { default: 32 }
@@ -12,39 +13,47 @@ AFRAME.registerComponent('send-process', {
 
     setTimeout(function () {
       if(document.getElementById("info01")){
-        document.getElementById("info01").setAttribute("visible","false")
+        //document.getElementById("info01").setAttribute("visible","false")
       }
       console.log("hide")
     }, delayInMilliseconds);
 
 
-    let firstExperience;
+    let firstExperience=false;
     let req = new XMLHttpRequest();
-    let aux_times_used = 0
+    
 
     //-----------------------------------------------------------------------------------
     //-------------------------------check experience exist------------------------------
     //-----------------------------------------------------------------------------------
 
-    req.open("POST", "https://api.medixlab.vxr.space/get_experience", true);
-    // req.open("POST", "http://localhost:8000/get_experience", true);
+    // req.open("POST", "https://api.medixlab.vxr.space/get_experience", true);
+    req.open("POST", "http://localhost:8000/get_experience", true);
     req.setRequestHeader('Content-Type', 'application/json');
     req.onload = function (e) {
 
       if (this.status == 200) {
-        console.log("response", this.response); // JSON response
+        console.log(this.response); // JSON response
+        
+        if (this.response === undefined) {
+          //experience didn't exist
+          firstExperience = true
+          console.log("firstExperience",firstExperience)
+          console.log("this.response: ",this.response)
+    
+        } else {
+        
+          obj = JSON.parse(this.responseText )
+          aux_times_used = obj.times_used
+          
+        }
       }
     }
     req.send(JSON.stringify({
-      //"user_id": "59c57f8f-b1a4-43d3-acaa-957deee29fed"
-      "user_id": localStorage.getItem("userID")
+      "user_id": "59c57f8f-b1a4-43d3-acaa-957deee29fed"
+      // "user_id": localStorage.getItem("userID")
     }));
-    if (this.response === undefined) {
-      //experience didn't exist
-
-    } else {
-      aux_times_used = this.response.times_used
-    }
+  
 
 
 
@@ -60,7 +69,7 @@ AFRAME.registerComponent('send-process', {
 
       if (e.detail.body.el.id == "e-Medixlab_Pen") {
 
-        if (boolean && firstExperience) {
+        if (boolean ) {
           boolean = false
           document.getElementById("lhand").removeAttribute("mixin")
           document.getElementById("rhand").removeAttribute("mixin")
@@ -71,8 +80,8 @@ AFRAME.registerComponent('send-process', {
           let initProcessTime = initProcess.toISOString().replace('Z', '')
           let endProcessTime = endProcess.toISOString().replace('Z', '')
           var xhr = new XMLHttpRequest();
-          xhr.open("POST", "https://api.medixlab.vxr.space/process", true);
-          // xhr.open("POST", "http://localhost:8000/process", true);
+          // xhr.open("POST", "https://api.medixlab.vxr.space/process", true);
+          xhr.open("POST", "http://localhost:8000/process", true);
           xhr.setRequestHeader('Content-Type', 'application/json');
           xhr.onload = function (e) {
 
@@ -98,8 +107,8 @@ AFRAME.registerComponent('send-process', {
 
           if (firstExperience) {
             let createExperience = new XMLHttpRequest();
-            createExperience.open("POST", "https://api.medixlab.vxr.space/experience", true);
-            // createExperience.open("POST", "http://localhost:8000/experience", true);
+            // createExperience.open("POST", "https://api.medixlab.vxr.space/experience", true);
+            createExperience.open("POST", "http://localhost:8000/experience", true);
             createExperience.setRequestHeader('Content-Type', 'application/json');
             createExperience.onload = function (e) {
               if (this.status == 200) {
@@ -108,15 +117,15 @@ AFRAME.registerComponent('send-process', {
             }
             createExperience.send(JSON.stringify(
               {
-                //"user_id": "59c57f8f-b1a4-43d3-acaa-957deee29fed",
-                "user_id": localStorage.getItem("userID"),
+                "user_id": "59c57f8f-b1a4-43d3-acaa-957deee29fed",
+                // "user_id": localStorage.getItem("userID"),
                 "times_used": 1
               }));
           } else {
             aux_times_used += 1;
             let updateExperience = new XMLHttpRequest();
-            updateExperience.open("PATCH", "https://api.medixlab.vxr.space/experience", true);
-            // updateExperience.open("PATCH", "http://localhost:8000/experience", true);
+            // updateExperience.open("PATCH", "https://api.medixlab.vxr.space/experience", true);
+            updateExperience.open("PATCH", "http://localhost:8000/experience", true);
             updateExperience.setRequestHeader('Content-Type', 'application/json');
             updateExperience.onload = function (e) {
               if (this.status == 200) {
@@ -125,8 +134,8 @@ AFRAME.registerComponent('send-process', {
             }
             updateExperience.send(JSON.stringify(
               {
-                //"user_id": "59c57f8f-b1a4-43d3-acaa-957deee29fed",
-                "user_id": localStorage.getItem("userID"),
+                "user_id": "59c57f8f-b1a4-43d3-acaa-957deee29fed",
+                // "user_id": localStorage.getItem("userID"),
                 "times_used": aux_times_used
               }));
 
